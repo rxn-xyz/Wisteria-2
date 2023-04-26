cbbf4fe28a1d00c45bdc79823f99114d6c057e2a
rxn-xyz <122142128+rxn-xyz@users.noreply.github.com>
2023-04-26 13:36:20 -0700

    Create Inf Spins.lua

---
+++ Inf Spins.lua
@@ -0,0 +1,45 @@
+-- Wisteria 2 | xyz
+-- Loader
+repeat task.wait() until game:IsLoaded() and game:GetService("Players"); task.wait(5); if game.PlaceId ~= 11573834824 then return end
+-- Globals
+getgenv().Clans = {"Kamado", "Tsugikuni"}
+getgenv().Webhook = {true, "https://discord.com/api/webhooks/1100263185698787469/Qi8RExILGtZkPQv53lslt8frWZWaewYDh8AIghAQX6EZy6BWHw-4D3miGDkCEXEbaJ8c"}
+-- Player Variables
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
+-- Wisteria 2 Variables
+local Clan = PlayerGui:WaitForChild("CC"):WaitForChild("Main"):WaitForChild("Clan")
+-- Data Rollback
+PlayerGui.Gui.Ui.UiModule.Modules.Settings.Set:InvokeServer(1, "\255")
+-- Reroll
+task.spawn(function()
+	while true do task.wait()
+		Clan.RRLastName.LocalScript.RR:InvokeServer("RRLastName"); task.wait(0.25)
+		-- Check Clan
+		print(Clan.LName.Text)
+		if table.find(Clans, Clan.LName.Text) then
+			-- Save Data
+			PlayerGui.Gui.Ui.UiModule.Modules.Settings.Set:InvokeServer(1, "B")
+			-- Webhook
+			if Webhook[1] then
+				local Data = {
+					["embeds"] = {{
+							["title"] = "**Clan Obtained**",
+							["description"] = "Clan: " .. Clan.LName.Text,
+							["type"] = "rich",
+							["color"] = tonumber(0x7269da),
+					}}
+				}
+				syn.request({Url = Webhook[2], Body = game:GetService("HttpService"):JSONEncode(Data), Method = "POST", Headers = {["content-type"] = "application/json"}})
+			end
+		break end
+		-- Check Spins
+		if Clan.Spins.Text == "0 Spins Left" then
+			for _ = 1, 3 do task.wait()
+				game:GetService("TeleportService"):Teleport(game.PlaceId, LocalPlayer); break
+			end
+		end
+	end
+end)
+-- End
